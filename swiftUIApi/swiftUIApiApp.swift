aa03baed096c1a7a1da66f36fac4925b75f582cf
Rodrigo-Youse <rodrigo.amancio@youse.com.br>
2024-06-14 13:33:59 -0300

    Initial Commit

---
+++ swiftUIApi/swiftUIApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIApiApp.swift
+//  swiftUIApi
+//
+//  Created by Rodrigo Silva on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
